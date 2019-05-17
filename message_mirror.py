d803cdfdb109e553ed6ea82a0648165992cb6fe0
Mattlau04 <37871312+Mattlau04@users.noreply.github.com>
2019-05-17 14:05:23 +0200

    will now clean mention, channel mention, etc...
    for exemple if i do @everyone in the mirrored channel it won't @everyone in the webhook channel

--- message_mirror.py
+++ message_mirror.py
@@ -1,5 +1,7 @@
 import discord
 from discord import Webhook, AsyncWebhookAdapter
+from discord import ext
+from discord.ext import commands
 import asyncio
 import sys
 import requests
@@ -7,7 +9,7 @@ import aiohttp
 import io
 
 client = discord.Client()
-token = "put ur token here"
+token = "ur token here"
 
 fromchannel = int(sys.argv[1])
 webtoken = sys.argv[2]
@@ -31,10 +33,10 @@ async def on_message(message):
                 await attachment.save(fp)
                 async with aiohttp.ClientSession() as session:
                     webhook = Webhook.from_url(webtoken, adapter=AsyncWebhookAdapter(session))
-                    await webhook.send(content=message.content, username=message.author.display_name, avatar_url=message.author.avatar_url, file=discord.File(fp, filename=attachment.filename))
+                    await webhook.send(content=message.clean_content, username=message.author.display_name, avatar_url=message.author.avatar_url, file=discord.File(fp, filename=attachment.filename))
         else:
             async with aiohttp.ClientSession() as session:
                 webhook = Webhook.from_url(webtoken, adapter=AsyncWebhookAdapter(session))
-                await webhook.send(content=message.content, username=message.author.display_name, avatar_url=message.author.avatar_url)
+                await webhook.send(content=message.clean_content, username=message.author.display_name, avatar_url=message.author.avatar_url)
 
 client.run(token, bot=False)
